c20045c2946ccba8c33cab0fa55e955976f19d37
Ingeniero Luis Grisales <contactoluiancol@gmail.com>
2026-06-09 16:57:43 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,22 +1,16 @@
 
-async function cargar(){
- const res = await fetch('data/dashboard.json');
- const data = await res.json();
+fetch('data/home.json')
+.then(r=>r.json())
+.then(data=>{
+document.getElementById('banner').innerHTML=`
+<div class="banner" style="background-image:url('${data.banner}')">
+<h1>${data.title}</h1>
+</div>`;
 
- document.getElementById('banner').innerHTML = `
- <div class="banner" style="background-image:url('${data.banner.imagen}')">
-   <div class="overlay">
-      <h1>${data.banner.titulo}</h1>
-      <p>${data.banner.subtitulo}</p>
-   </div>
- </div>`;
-
- document.getElementById('cards').innerHTML =
- data.cards.map(c=>`
- <div class="card">
-   <i class="fa-solid ${c.icono}"></i>
-   <h3>${c.titulo}</h3>
-   <p>${c.descripcion}</p>
- </div>`).join('');
-}
-cargar();
+document.getElementById('cards').innerHTML=data.cards.map(c=>`
+<div class="card" onclick="location.href='pages/repository.html'">
+<h2>${c.name}</h2>
+<p>${c.description}</p>
+</div>
+`).join('');
+});
