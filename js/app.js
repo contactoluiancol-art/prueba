fd872e3515e505f3a9eaff58feb4053733850432
Ingeniero Luis Grisales <contactoluiancol@gmail.com>
2026-06-11 08:54:55 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-fetch('data/modules.json').then(r=>r.json()).then(data=>{document.getElementById('banner').innerHTML=`<div class="banner" style="background-image:url('${data.banner.image}')"><h1>${data.banner.title}</h1><p>${data.banner.subtitle}</p></div>`;document.getElementById('modules').innerHTML=data.modules.map(m=>`<div class="card" onclick="location.href='pages/module.html'"><h3>${m.title}</h3><p>${m.description}</p></div>`).join('')})
+
+async function cargar(){
+ const res = await fetch('data/dashboard.json');
+ const data = await res.json();
+
+ document.getElementById('banner').innerHTML = `
+ <div class="banner" style="background-image:url('${data.banner.imagen}')">
+   <div class="overlay">
+      <h1>${data.banner.titulo}</h1>
+      <p>${data.banner.subtitulo}</p>
+   </div>
+ </div>`;
+
+ document.getElementById('cards').innerHTML =
+ data.cards.map(c=>`
+ <div class="card">
+   <i class="fa-solid ${c.icono}"></i>
+   <h3>${c.titulo}</h3>
+   <p>${c.descripcion}</p>
+ </div>`).join('');
+}
+cargar();
