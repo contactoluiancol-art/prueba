bfc55ef89bd635a0825ceb870cccc296ebcece39
Ingeniero Luis Grisales <contactoluiancol@gmail.com>
2026-06-12 10:29:21 -0500

    Delete backend directory

--- backend/server.js
+++
@@ -1,4 +0,0 @@
-const express=require('express');
-const app=express();
-app.get('/',(req,res)=>res.json({mensaje:'API EI SST funcionando'}));
-app.listen(3000,()=>console.log('Servidor iniciado'));
